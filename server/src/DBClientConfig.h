12b31d41c241828b3f34acc0f34038beec77560f
Takuro Ashie <ashie@clear-code.com>
2013-07-11 10:23:18 +0900

    Add a config item to enable/disable on-demand copy

--- server/src/DBClientConfig.h
+++ server/src/DBClientConfig.h
@@ -82,6 +82,7 @@ public:
 	bool isFaceMySQLEnabled(void);
 	int  getFaceRestPort(void);
 	void setFaceRestPort(int port);
+	bool isCopyOnDemandEnabled(void);
 	void addTargetServer(MonitoringServerInfo *monitoringServerInfo);
 	void getTargetServers(MonitoringServerInfoList &monitoringServers);
 
